016b1a0ae189a981c171928ed5063707fa5f4cb3
faizunnur087 <faizunnur087@gmail.com>
2026-04-10 15:07:42 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/test/java/com/example/dockertst1/Dockertst1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockertst1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dockertst1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
